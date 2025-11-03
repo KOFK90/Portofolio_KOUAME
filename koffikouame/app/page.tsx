a5399e0e8f7cbf7f555d855065e930a2bb5670df
KOFK90 <koffi.kouame@epitech.eu>
2025-11-03 01:25:06 +0000

    [ADD]:Navbar_and_Footer

---
+++ koffikouame/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
+
+const Home = () => {
+  return (
+    <><Navbar></Navbar><Footer></Footer></>
+  )
+}
+
+export default Home;
